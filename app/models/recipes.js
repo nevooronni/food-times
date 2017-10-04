1cf00aecbef2488f3ede0e64235a366f54736b6f
Philip Weru <philip.weru@gmail.com>
2017-10-04 09:45:53 +0300

    To make changes to firebase

--- app/models/recipes.js
+++ app/models/recipes.js
@@ -1,6 +1,6 @@
-import Model from 'ember-data/model';
+import DS from 'ember-data';
 
-export default Model.extend({
+export default DS.Model.extend({
   foodName: DS.attr(),
   ingredients: DS.attr(),
   instructions: DS.attr(),
