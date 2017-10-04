8c5273851bcf365e2c3e148228984f79f838b43d
Philip Weru <philip.weru@gmail.com>
2017-10-04 10:49:37 +0300

    To make additions to js and hbs files

--- app/routes/index.js
+++ app/routes/index.js
@@ -4,4 +4,12 @@ export default Ember.Route.extend ({
   model() {
   return this.store.findAll('recipes');
   },
+
+  actions: {
+      saveRecipe3(params) {
+        var newRecipe = this.store.createRecord('recipes', params);
+        newRecipe.save();
+        this.transitionTo('index');
+      }
+    }
 });
